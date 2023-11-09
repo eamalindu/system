7a28d2f1b4eaf464e3dc53823962701db533e152
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-09 13:41:46 +0530

    edited

--- system/src/main/java/lk/steam/system/inquiryStatus.java
+++ system/src/main/java/lk/steam/system/inquiryStatus.java
@@ -1,4 +1,19 @@
 package lk.steam.system;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "inquirystatus")
+@Data
 public class inquiryStatus {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name",length = 45)
+    private String name;
+
 }
