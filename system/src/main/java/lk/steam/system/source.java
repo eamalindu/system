7662d1c470cb2a89f49b751a248129954f353006
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-09 12:25:17 +0530

    edited

--- system/src/main/java/lk/steam/system/source.java
+++ system/src/main/java/lk/steam/system/source.java
@@ -1,4 +1,16 @@
 package lk.steam.system;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "source")
+@Data
 public class source {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
 }
