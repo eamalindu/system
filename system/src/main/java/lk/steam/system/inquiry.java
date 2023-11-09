0b2f0f645e416289229d9ea3ca5469b45bc3fd9f
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-09 11:03:05 +0530

    edited

--- system/src/main/java/lk/steam/system/inquiry.java
+++ system/src/main/java/lk/steam/system/inquiry.java
@@ -1,13 +1,32 @@
 package lk.steam.system;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class inquiry {
 
     private Integer id;
 
-    private String firstname;
+    private String firstName;
 
-    private String lastname;
+    private String lastName;
 
+    private Integer primaryMobileNumber;
 
+    private Integer secondaryMobileNumber;
+
+    private String email;
+
+    private String idType;
+
+    private String idValue;
+
+    private String description;
+
+    private String addedBy;
+
+    private LocalDateTime timeStamp;
+
+    //add foreign keys now
 
 }
