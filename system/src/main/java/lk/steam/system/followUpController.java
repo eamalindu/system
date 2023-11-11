f535bb508a6a947a95c01dd04c0e897e028f1721
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-11 16:16:38 +0530

    edited

--- system/src/main/java/lk/steam/system/followUpController.java
+++ system/src/main/java/lk/steam/system/followUpController.java
@@ -1,4 +1,9 @@
 package lk.steam.system;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/STEAM-CRM/followup")
 public class followUpController {
 }
