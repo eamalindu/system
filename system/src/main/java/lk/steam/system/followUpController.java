ea481ad689c5d0bc0f6e4e7e83c04931e080117c
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-11 16:17:25 +0530

    edited

--- system/src/main/java/lk/steam/system/followUpController.java
+++ system/src/main/java/lk/steam/system/followUpController.java
@@ -1,9 +1,20 @@
 package lk.steam.system;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/STEAM-CRM/followup")
 public class followUpController {
+    @Autowired
+    private followUpDAO followUpDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<inquiry> findAll(){
+        return inquiryDAO.findAll();
+    }
 }
