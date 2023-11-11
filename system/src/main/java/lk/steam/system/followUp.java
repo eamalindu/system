92fa9fc12d1dadd710352d577cbd1a0c08f1ca34
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-11 16:11:15 +0530

    edited

--- system/src/main/java/lk/steam/system/followUp.java
+++ system/src/main/java/lk/steam/system/followUp.java
@@ -25,16 +25,28 @@ public class followUp {
     @NotNull
     private String type;
 
+    @Column(name = "feeling")
+    @NotNull
     private String feeling;
 
+    @Column(name = "confirmed")
+    @NotNull
     private String confirmed;
 
+    @Column(name = "content")
+    @NotNull
     private String content;
 
+    @Column(name = "contacttime")
+    @NotNull
     private LocalDateTime contactTime;
 
+    @Column(name = "addedby")
+    @NotNull
     private String addedBy;
 
+    @Column(name = "followuptime")
+    @NotNull
     private LocalDateTime followUpTime;
 
     //foreign key
