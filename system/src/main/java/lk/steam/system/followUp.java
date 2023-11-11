b95c32f9799ace0acf00a7ff2b4161a08f2f22bd
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-11 15:20:49 +0530

    edited

--- system/src/main/java/lk/steam/system/followUp.java
+++ system/src/main/java/lk/steam/system/followUp.java
@@ -1,7 +1,16 @@
 package lk.steam.system;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "followup")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class followUp {
 }
