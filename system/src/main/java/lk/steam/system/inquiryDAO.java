f4a9c85103e8dacf152742298faba0b7568e16d5
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-12 21:30:37 +0530

    edited

--- system/src/main/java/lk/steam/system/inquiryDAO.java
+++ system/src/main/java/lk/steam/system/inquiryDAO.java
@@ -17,7 +17,10 @@ public interface inquiryDAO extends JpaRepository<inquiry,Integer> {
     @Query(value = "SELECT * FROM inquiry WHERE inquiryStatus_id=4",nativeQuery = true)
     List<inquiry> findDroppedInquiry();
 
-    @Query(value="SELECT DISTINCT i.id, i.source_id, i.course_id, i.firstname, i.lastname, i.primarymobilenumber, i.secondarymobilenumber, i.email, i.idtype, i.idvalue, i.contacttime, i.description,i.addedby,i.timestamp, i.inquirystatus_id, f.type, f.feeling, f.confirmed, f.content, f.contacttime AS followuptime FROM inquiry i LEFT JOIN followup f ON i.id = f.inquiry_id\n" +
+    @Query(value = "SELECT * FROM inquiry WHERE inquiry.inquirystatus_id =1;",nativeQuery = true)
+    List<inquiry> findNewInquiry();
+
+    @Query(value="SELECT DISTINCT i.id, i.source_id, i.course_id, i.firstname, i.lastname, i.primarymobilenumber, i.secondarymobilenumber, i.email, i.idtype, i.idvalue, i.contacttime, i.description,i.addedby,i.timestamp, i.inquirystatus_id, f.type, f.feeling, f.confirmed, f.content, f.nextfollowup AS followuptime FROM inquiry i LEFT JOIN followup f ON i.id = f.inquiry_id\n" +
             "WHERE f.followuptime = (SELECT MAX(followuptime) FROM followup WHERE followup.inquiry_id = i.id) ORDER BY i.id;",nativeQuery = true)
     List<inquiry> test();
 
