289f6b14d634aa585eba18d0f7e1c0ef40ff0b9b
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-15 13:01:29 +0530

    DAO file added for Source

---
+++ system/src/main/java/lk/steam/system/dao/SourceDAO.java
@@ -0,0 +1,7 @@
+package lk.steam.system.dao;
+
+import lk.steam.system.entity.Source;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SourceDAO extends JpaRepository<Source,Integer> {
+}
