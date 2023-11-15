2f3c01aae46a287b93d8644448348a5c650a1ee6
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-15 13:04:58 +0530

    controller fle for course

---
+++ system/src/main/java/lk/steam/system/controller/CourseController.java
@@ -0,0 +1,24 @@
+package lk.steam.system.controller;
+
+import lk.steam.system.dao.CourseDAO;
+import lk.steam.system.entity.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/Course")
+public class CourseController {
+
+    @Autowired
+    private CourseDAO courseDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Course> findAll(){
+        return courseDAO.findAll();
+    }
+
+}
