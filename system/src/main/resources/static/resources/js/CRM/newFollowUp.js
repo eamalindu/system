be294160a275110099d51eb15b3c2693f7202810
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-18 13:39:08 +0530

    folowup implemented

---
+++ system/src/main/resources/static/resources/js/CRM/newFollowUp.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+
+    newFollowUp = {};
+
+    //external libraries initialization
+    $('#inquiryFollowUpTime').daterangepicker({
+        "minDate": new Date(),
+        "singleDatePicker": true,
+        "timePicker": true,
+        "timePicker24Hour": true,
+        "autoApply": true,
+        "linkedCalendars": false,
+        "showCustomRangeLabel": false,
+        "drops": "up",
+        "locale": {
+            "format": "YYYY-MM-DD HH:mm"
+        }
+    });
+
+    //validation for chosen select (for inquiry follow-up)
+    $("#inquiryFollowUpType").chosen().change(function () {
+        $("#inquiryFollowUpType_chosen .chosen-single").addClass('select-validated');
+    });
+
+});
+
+const newFollowUpSubmit = ()=>{
+    console.log(newFollowUp)
+}
