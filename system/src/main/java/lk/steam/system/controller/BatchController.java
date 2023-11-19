158bc427805ad098e45c0ea439ed5298fde818e9
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-19 21:51:41 +0530

    edited

--- system/src/main/java/lk/steam/system/controller/BatchController.java
+++ system/src/main/java/lk/steam/system/controller/BatchController.java
@@ -1,5 +1,7 @@
 package lk.steam.system.controller;
 
+import lk.steam.system.dao.BatchDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/batch")
 public class BatchController {
 
+    @Autowired
+    private BatchDAO batchDAO;
 }
