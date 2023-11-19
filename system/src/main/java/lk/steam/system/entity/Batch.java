2cc21f213bda8df1533ffac78770bcb3ddc627c4
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-19 20:52:26 +0530

    properties added

--- system/src/main/java/lk/steam/system/entity/Batch.java
+++ system/src/main/java/lk/steam/system/entity/Batch.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Entity
 @Table(name = "batch")
 
@@ -18,17 +22,20 @@ public class Batch {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String batchcode varchar(45)
-    private String commencedate date
-    private String enddate date
-    private String seatcount int
-    private String starttime time
-    private String endtime time
-    private String createdby varchar(45)
-    private String lecturer varchar(45)
-    private String status int
-    private String timestamp datetime
-    private String location varchar(45)
-    private String description varchar(255)
-    private String course_id int
+    private String batchcode;
+    private LocalDate commencedate;
+    private LocalDate enddate;
+    private String seatcount;
+    private LocalTime starttime;
+    private LocalTime endtime;
+    private String createdby;
+    private String lecturer;
+    private Integer status;
+    private LocalDateTime timestamp;
+    private String location;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn()
+    private Course course_id;
 }
