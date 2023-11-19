14fa448dd4022f406162b75bcf2572d005188451
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-19 20:43:56 +0530

    lombok added

--- system/src/main/java/lk/steam/system/entity/Batch.java
+++ system/src/main/java/lk/steam/system/entity/Batch.java
@@ -1,11 +1,34 @@
 package lk.steam.system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "batch")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Batch {
 
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private String batchcode varchar(45)
+    private String commencedate date
+    private String enddate date
+    private String seatcount int
+    private String starttime time
+    private String endtime time
+    private String createdby varchar(45)
+    private String lecturer varchar(45)
+    private String status int
+    private String timestamp datetime
+    private String location varchar(45)
+    private String description varchar(255)
+    private String course_id int
 }
