2aeaee6ec8e79b51930185e73661421033a6c022
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-11-19 21:55:12 +0530

    findall implemented

--- system/src/main/java/lk/steam/system/controller/BatchController.java
+++ system/src/main/java/lk/steam/system/controller/BatchController.java
@@ -1,9 +1,13 @@
 package lk.steam.system.controller;
 
 import lk.steam.system.dao.BatchDAO;
+import lk.steam.system.entity.Batch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/batch")
@@ -11,4 +15,9 @@ public class BatchController {
 
     @Autowired
     private BatchDAO batchDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Batch> findAll(){
+        return batchDAO.findAll();
+    }
 }
