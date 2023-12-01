f98d2a2cab0ba7c083b25a9bf1974de869417748
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-12-01 14:37:20 +0530

    edited

--- system/src/main/java/lk/steam/system/controller/FollowUpController.java
+++ system/src/main/java/lk/steam/system/controller/FollowUpController.java
@@ -21,39 +21,55 @@ public class FollowUpController {
     private InquiryDAO inquiryDAO;
 
 
-    @GetMapping(value = "/findall",produces = "application/json")
-    public List<FollowUp> findAll(){
+    @GetMapping(value = "/findall", produces = "application/json")
+    public List<FollowUp> findAll() {
         return followUpDAO.findAll();
     }
 
-    @GetMapping(value = "/latestFollowup",produces = "application/json")
-    public List<FollowUp> latestFollowupForEachInquiry(){
+    @GetMapping(value = "/latestFollowup", produces = "application/json")
+    public List<FollowUp> latestFollowupForEachInquiry() {
         return followUpDAO.latestFollowupForEachInquiry();
     }
 
     @PostMapping
-    public String saveNewFollowup(@RequestBody FollowUp followUp){
+    public String saveNewFollowup(@RequestBody FollowUp followUp) {
 
-        try{
+        try {
 
-            //get inquiryID from the followUp object
-            Inquiry currentInquiry = inquiryDAO.getReferenceById(followUp.getInquiryId().getId());
-            //change inquiry status to 2
-            currentInquiry.setInquiryStatusId(new InquiryStatus(2,"Processing"));
+            if (followUp.getInquiryId().getInquiryStatusId().getId()==1) {
+                //get inquiryID from the followUp object
+                Inquiry currentInquiry = inquiryDAO.getReferenceById(followUp.getInquiryId().getId());
+                //change inquiry status to 2
+                currentInquiry.setInquiryStatusId(new InquiryStatus(2, "Processing"));
+                inquiryDAO.save(currentInquiry);
 
-            inquiryDAO.save(currentInquiry);
+                //set auto generated values
+                followUp.setFollowUpTime(LocalDateTime.now());
+                followUp.setAddedBy("User1");
 
-            //set auto generated values
-            followUp.setFollowUpTime(LocalDateTime.now());
-            followUp.setAddedBy("User1");
+                followUpDAO.save(followUp);
 
-            followUpDAO.save(followUp);
+                return "OK";
 
-            return "OK";
+            } else if (followUp.getInquiryId().getInquiryStatusId().getId() == 3) {
+                return "This Inquiry is Already Registered!";
 
-        }
-        catch (Exception ex){
-            return "Save Failed "+ex.getMessage();
+            } else if (followUp.getInquiryId().getInquiryStatusId().getId() == 4) {
+                return "This Inquiry is Dropped!";
+            } else {
+                //set auto generated values
+                followUp.setFollowUpTime(LocalDateTime.now());
+                followUp.setAddedBy("User1");
+
+                followUpDAO.save(followUp);
+
+                return "OK"+followUp.getInquiryId().getId();
+
+            }
+
+
+        } catch (Exception ex) {
+            return "Save Failed " + ex.getMessage();
         }
 
     }
