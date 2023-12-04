340c783915f932e26bc4f12175e461524107f180
E A Malindu Prabodhitha <64830201+eamalindu@users.noreply.github.com>
2023-12-04 20:40:03 +0530

    exportToExcel function added

--- system/src/main/resources/static/resources/js/external-exportData.js
+++ system/src/main/resources/static/resources/js/external-exportData.js
@@ -1,3 +1,26 @@
-const exportToExcel = () => {
+const exportToExcel = (jsonData, fileName, columns) => {
+    var filteredData = jsonData.map(item => {
+        var filteredItem = {};
+        columns.forEach(column => {
+            var fieldName = column.name;
+            var nestedProperties = column.data.split('.');
+            // Traverse the nested properties to get the final value
+            var value = item;
+            for (var i = 0; i < nestedProperties.length; i++) {
+                value = value[nestedProperties[i]];
+                if (value === undefined) {
+                    // Handle undefined values to prevent errors
+                    value = null;
+                    break;
+                }
+            }
+            filteredItem[fieldName] = value;
+        });
+        return filteredItem;
+    });
 
+    var ws = XLSX.utils.json_to_sheet(filteredData);
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
+    XLSX.writeFile(wb, "Inquiries from"+fileName + ".xlsx");
 }
