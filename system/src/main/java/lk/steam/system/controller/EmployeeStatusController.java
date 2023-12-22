6f6978c76f3d55de0648d90779cdc6c8c1ef0d38
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:46:48 +0530

    EmployeeStatusController implemented

--- system/src/main/java/lk/steam/system/controller/EmployeeStatusController.java
+++ system/src/main/java/lk/steam/system/controller/EmployeeStatusController.java
@@ -1,4 +1,23 @@
 package lk.steam.system.controller;
 
+import lk.steam.system.dao.EmployeeStatusDAO;
+import lk.steam.system.entity.EmployeeStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employeestatus")
 public class EmployeeStatusController {
+
+    @Autowired
+    private EmployeeStatusDAO employeeStatusDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<EmployeeStatus> findAll(){
+        return employeeStatusDAO.findAll();
+    }
 }
