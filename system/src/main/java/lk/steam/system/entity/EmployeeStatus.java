d5cbafaef36b5633b7769d37a1c4da16f74393d9
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:16:14 +0530

    EmployeeStatus table mapped

--- system/src/main/java/lk/steam/system/entity/EmployeeStatus.java
+++ system/src/main/java/lk/steam/system/entity/EmployeeStatus.java
@@ -1,4 +1,25 @@
 package lk.steam.system.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",unique = true)
+    private Integer id;
+
+    @Column(name = "status")
+    @NotNull
+    private String status;
+
+
 }
