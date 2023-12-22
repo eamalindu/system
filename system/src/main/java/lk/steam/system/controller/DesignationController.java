784c5a1ee272cf63abc58241792e49e34e05d21b
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 12:01:58 +0530

    findAll service mapped

--- system/src/main/java/lk/steam/system/controller/DesignationController.java
+++ system/src/main/java/lk/steam/system/controller/DesignationController.java
@@ -1,4 +1,23 @@
 package lk.steam.system.controller;
 
+import lk.steam.system.dao.DesignationDAO;
+import lk.steam.system.entity.Designation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/designation")
 public class DesignationController {
+
+    @Autowired
+    private DesignationDAO designationDAO;
+
+    @GetMapping(value = "/findall",produces = "application/json")
+    public List<Designation> findAll(){
+        return designationDAO.findAll();
+    }
 }
