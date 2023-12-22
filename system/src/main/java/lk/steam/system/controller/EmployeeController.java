62b4a324b0cc48c6d6eb8b2e5b3665bdf5ae665f
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:36:25 +0530

    findAll service implemented

--- system/src/main/java/lk/steam/system/controller/EmployeeController.java
+++ system/src/main/java/lk/steam/system/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package lk.steam.system.controller;
 
+import lk.steam.system.dao.EmployeeDAO;
+import lk.steam.system.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @GetMapping(value = "/employee/findall")
+    public List<Employee> findAll(){
+
+        return employeeDAO.findAll();
+    }
+
 }
