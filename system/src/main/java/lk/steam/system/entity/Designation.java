705b4fdf46f5928b079658678a9cdb5bdd6d0c90
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:51:02 +0530

    table name added

--- system/src/main/java/lk/steam/system/entity/Designation.java
+++ system/src/main/java/lk/steam/system/entity/Designation.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "designation")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- system/src/main/java/lk/steam/system/entity/EmployeeStatus.java
+++ system/src/main/java/lk/steam/system/entity/EmployeeStatus.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "employeestatus")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
