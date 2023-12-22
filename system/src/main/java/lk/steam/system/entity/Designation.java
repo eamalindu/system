3aae2c30b0308a17a3446f50c663e34af8a21d99
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:21:55 +0530

    Designation table mapped

--- system/src/main/java/lk/steam/system/entity/Designation.java
+++ system/src/main/java/lk/steam/system/entity/Designation.java
@@ -1,4 +1,23 @@
 package lk.steam.system.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Designation {
+
+    @Id
+    @Column(name = "id",unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "designation")
+    @NotNull
+    private String designation;
 }
