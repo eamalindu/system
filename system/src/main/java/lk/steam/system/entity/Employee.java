b5a5434791c15fa5e6c6a284444775798e71b899
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 10:59:56 +0530

    Employee Table mapped

--- system/src/main/java/lk/steam/system/entity/Employee.java
+++ system/src/main/java/lk/steam/system/entity/Employee.java
@@ -1,4 +1,77 @@
 package lk.steam.system.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "employeeid", unique = true)
+    @NotNull
+    private String employeeID;
+
+    @Column(name = "fullname")
+    @NotNull
+    private String fullName;
+
+    @Column(name = "callingname")
+    @NotNull
+    private String callingName;
+
+    @Column(name = "nic", length = 10)
+    @NotNull
+    private String nic;
+
+    @Column(name = "dob")
+    @NotNull
+    private LocalDate dob;
+
+    @Column(name = "gender")
+    @NotNull
+    private String gender;
+
+    @Column(name = "email",unique = true)
+    @NotNull
+    private String email;
+
+    @Column(name = "mobilenumber",length = 10)
+    @NotNull
+    private String mobileNumber;
+
+    @Column(name = "landnumber")
+    private String landNumber;
+
+    @Column(name = "address")
+    @NotNull
+    private String address;
+
+    @Column(name = "civilstatus")
+    @NotNull
+    private String civilStatus;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "added_timestamp")
+    @NotNull
+    private LocalDateTime added_timestamp;
+
+
+    private Integer employeeStatusID;
+
+    private Integer designationID;
 }
