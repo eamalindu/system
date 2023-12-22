782905e88751999260415d5ff156b74b188f855e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 14:09:23 +0530

    edited

--- system/src/main/resources/static/resources/js/CRM/employee.js
+++ system/src/main/resources/static/resources/js/CRM/employee.js
@@ -1,5 +1,16 @@
 window.addEventListener('load',()=>{
 
+    //new employee object
+    newEmployee = {};
+
+    //dynamic select for courses
+    Status = ajaxGetRequest("/employeestatus/findall");
+    fillSelectOptions(employeeStatus, 'Please Select a Status', Status, 'status')
+
+    //dynamic select for sources
+    designations = ajaxGetRequest("/designation/findall")
+    fillSelectOptions(employeeDesignation, 'Please Select a Designation', designations, 'designation')
+
     //initializing 3rd party libraries
     $('#employeeDOB').daterangepicker({
         "maxDate": new Date(),
@@ -14,7 +25,24 @@ window.addEventListener('load',()=>{
             "format": "YYYY-MM-DD"
         }
     });
-    $('#employeeDesignation').chosen();
-    $('#employeeCivilStatus').chosen();
-    $('#employeeStatus').chosen();
+    $('#employeeDesignation').chosen({width: '100%'});
+    $('#employeeCivilStatus').chosen({width: '100%'});
+    $('#employeeStatus').chosen({width: '100%'});
+
+    refreshEmployeeTable()
+
+    //validation chosen select (for new employee)
+    $("#employeeCivilStatus").chosen().change(function () {
+        $("#employeeCivilStatus_chosen .chosen-single").addClass('select-validated');
+    });
+    $("#employeeDesignation").chosen().change(function () {
+        $("#employeeDesignation_chosen .chosen-single").addClass('select-validated');
+    });
+    $("#employeeStatus").chosen().change(function () {
+        $("#employeeStatus_chosen .chosen-single").addClass('select-validated');
+    });
 })
+
+const refreshEmployeeTable = ()=>{
+
+}
