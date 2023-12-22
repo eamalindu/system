2ff0e319d664cbc4c3fed4bc9113be281d7574da
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-22 11:37:22 +0530

    class level mapping added

--- system/src/main/java/lk/steam/system/controller/EmployeeController.java
+++ system/src/main/java/lk/steam/system/controller/EmployeeController.java
@@ -4,17 +4,19 @@ import lk.steam.system.dao.EmployeeDAO;
 import lk.steam.system.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/employee")
 public class EmployeeController {
 
     @Autowired
     private EmployeeDAO employeeDAO;
 
-    @GetMapping(value = "/employee/findall")
+    @GetMapping(value = "/findall")
     public List<Employee> findAll(){
 
         return employeeDAO.findAll();
