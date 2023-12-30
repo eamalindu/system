68197c89daf76ca4c3d0e9d3fe72c0d4b80e1a93
E A Malindu Prabodhitha <eamalindu@gmail.com>
2023-12-30 19:53:30 +0530

    updateEmployee service updated

--- system/src/main/java/lk/steam/system/controller/EmployeeController.java
+++ system/src/main/java/lk/steam/system/controller/EmployeeController.java
@@ -2,10 +2,9 @@ package lk.steam.system.controller;
 
 import lk.steam.system.dao.EmployeeDAO;
 import lk.steam.system.entity.Employee;
+import lk.steam.system.entity.Inquiry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,19 @@ public class EmployeeController {
     public List<Employee> findAll(){
 
         return employeeDAO.findAll();
+    }
+    @PutMapping
+    public String updateEmployee(@RequestBody Employee employee){
+        try {
+            //no need to check anything, because there are no any unique values
+            employeeDAO.save(employee);
+            return "OK";
+
+        }
+        catch (Exception ex){
+            return "Update Failed "+ex.getMessage();
+        }
+
     }
 
 }
